fda799fb712474eaebaa897b22f662803dc3146e
oweidat05 <wmhammad4486@gmail.com>
2025-08-12 04:51:37 -0400

    Add files via upload

---
+++ ft_print_cosmb.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+static void	print_comb(int a, int b, int c)
+{
+	ft_putchar(a + '0');
+	ft_putchar(b + '0');
+	ft_putchar(c + '0');
+	if (!(a == 7 && b == 8 && c == 9))
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = 0;
+	while (a <= 7)
+	{
+		b = a + 1;
+		while (b <= 8)
+		{
+			c = b + 1;
+			while (c <= 9)
+				print_comb(a, b, c++);
+			b++;
+		}
+		a++;
+	}
+}
